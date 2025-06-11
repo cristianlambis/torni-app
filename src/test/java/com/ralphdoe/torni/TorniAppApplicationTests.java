2e9b2b84a91897c1c80f35c93e218af187e90985
Cristian Lambis <cristianlambis09@gmail.com>
2025-06-11 14:29:25 -0500

    Initialize hexagonal folder structure

---
+++ src/test/java/com/ralphdoe/torni/TorniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ralphdoe.torni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
